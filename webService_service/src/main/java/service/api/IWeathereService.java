49afea4570eb50bc0feb22a5671733e26034189a
fengmenglei <fengmenglei@vortexinfo.cn>
2019-03-21 17:55:03 +0800

    环境接口

---
+++ webService_service/src/main/java/service/api/IWeathereService.java
@@ -0,0 +1,12 @@
+package service.api;
+
+import javax.jws.WebService;
+
+/**
+ * @description:
+ * @author: fengmenglei
+ * @date: 2019/3/21
+ */
+@WebService
+public interface IWeathereService {
+}
---
+++ webService_service/src/main/java/service/impl/WeatherServiceImpl.java
@@ -0,0 +1,9 @@
+package service.impl;
+
+/**
+ * @description:
+ * @author: fengmenglei
+ * @date: 2019/3/21
+ */
+public class WeatherServiceImpl {
+}
