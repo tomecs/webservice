717638460b372afc6f63932b5338115366284766
fengmenglei <fengmenglei@vortexinfo.cn>
2019-03-21 19:53:51 +0800

    创建客户端

--- webService_service/src/test/fml/Server.java
+++ webService_service/src/test/fml/Server.java
@@ -1,5 +1,9 @@
 package fml;
 
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.security.JAASLoginInterceptor;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.jaxws.handler.logical.LogicalHandlerInInterceptor;
 import service.impl.WeatherServiceImpl;
 
 import javax.xml.ws.Endpoint;
@@ -12,7 +16,20 @@ import javax.xml.ws.Endpoint;
 public class Server {
 
     public static void main(String[] args) {
-        Endpoint.publish("http://192.168.17.10:8078/weather", new WeatherServiceImpl());
+
+        // 设置发布服务的工厂
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        // 设置服务地址
+        factory.setAddress("http://192.168.17.10:8078/ws/");
+
+        // 设置服务类
+        factory.setServiceBean(new WeatherServiceImpl());
+
+        // 添加日志输入输出拦截器
+        factory.getInInterceptors().add(new JAASLoginInterceptor());
+        factory.getOutInterceptors().add(new JAASLoginInterceptor());
+        // 发布服务
+        factory.create();
         System.out.println("发布成功");
     }
 
